786de81f47de21480aeea82ac26718fc10d25f3f
Dmitry V. Luciv <dluciv@dluciv.name>
2026-03-29 23:08:47 +0300

    Более содержательный интеграционный тест

--- 00.Demo/calculator.py
+++ 00.Demo/calculator.py
@@ -7,4 +7,8 @@ import sys
 import mean
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Provide at least one number!", file=sys.stderr)
+        sys.exit(1)
+
     print(mean.calculate_mean(list(map(float, sys.argv[1:]))))
--- 00.Demo/integration_test.sh
+++ 00.Demo/integration_test.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+status=true
+
+# ---------------------------------------------------
+
 result=$(uv run python -O ./calculator.py 1 2 3)
 
 if [[ $result == "2.0" ]]; then
-  echo Ok!
+  echo "Calculates mean"
 else
-  echo Fail!
-  exit 1
+  echo "Fails to calculate mean" >&2
+  status=false
 fi
+
+# ---------------------------------------------------
+
+uv run python -O ./calculator.py
+err=$?
+
+if [[ $err == "1" ]]; then
+  echo "Returns 1 on empty args"
+else
+  echo "Does not return 1 on empty args" >&2
+  status=false
+fi
+
+$status
