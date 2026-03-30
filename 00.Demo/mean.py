0157b3f7168f3e757e63c722cb941ec0f1ac2a66
Dmitry V. Luciv <dluciv@dluciv.name>
2026-03-30 12:31:55 +0300

    pylint rulez

--- 00.Demo/mean.py
+++ 00.Demo/mean.py
@@ -9,6 +9,6 @@ from beartype import beartype
 @beartype
 def calculate_mean(numbers: list[int | float]) -> float:
     """Calculate mean with type checking"""
-    if not len(numbers):
+    if len(numbers) == 0:
         raise ValueError()
     return float(np.mean(numbers))
